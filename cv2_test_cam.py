16cee2468540b66fce3a69a9ba3175e17fee8320
Sergey Zobin <zobin.sergey@gmail.com>
2020-03-21 11:35:00 -0600

    + test cv2 - camera (update 2)

--- cv2_test_cam.py
+++ cv2_test_cam.py
@@ -1,17 +1,7 @@
-import cv2_tools
-from cv2_tools.Management import ManagerCV2
-import cv2
+# cv2 camera test
 
-print('Name: {}\nVersion:{}\nHelp:{}'.format(cv2_tools.name,cv2_tools.__version__,cv2_tools.help))
-
-# keystroke=27 is the button `esc`
-manager_cv2 = ManagerCV2(cv2.VideoCapture(0), is_stream=True, fps_limit=60)
-
-# This for will manage file descriptor for you
-for frame in manager_cv2:
-    # Each time you press a button, you will get its id in your terminal
-    last_keystroke = manager_cv2.get_last_keystroke()
-    if last_keystroke != -1:
-        print(last_keystroke)
-    cv2.imshow('Easy button checker', frame)
-cv2.destroyAllWindows()
+from cv2 import VideoCapture, imwrite
+cam = VideoCapture(0)
+s, img = cam.read()
+if s:
+    imwrite("filename.jpg", img)
